ef7841345ca79bc5cbe385a82fdc2ae9cf40ca9a
Kacper <kacper77xx@gmail.com>
2023-04-12 12:55:47 +0200

    First commit

---
+++ Chapter005.py
@@ -0,0 +1,16 @@
+# n1 = int(input('First number: '))
+# n2 = int(input('Second number: '))
+# n3 = int(input('Third number: '))
+# n4 = int(input('Fourth number: '))
+# n5 = int(input('Fifth number: '))
+# sumOfNumbers = n1 + n2 + n3 + n4 + n5
+# print(sumOfNumbers)
+
+n1 = int(input('First number: '))
+n2 = int(input('Second number: '))
+n3 = int(input('Third number: '))
+n4 = int(input('Fourth number: '))
+n5 = int(input('Fifth number: '))
+n6 = int(input('Sixth number: '))
+averageOfNumbers = (n1 + n2 + n3 + n4 + n5 + n6)/6
+print('Average of numbers you typed: ' + str(round(averageOfNumbers, 2)))
