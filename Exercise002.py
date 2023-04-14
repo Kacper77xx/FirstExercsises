40589f67cc289e8bef20195e6e0b87630249be0a
Kacper <kacper77xx@gmail.com>
2023-04-14 18:39:42 +0200

    First commit

--- Exercise002.py
+++ Exercise002.py
@@ -1,5 +1,36 @@
-list = ['a', 'b', 'c', 'd']
-for i in range(0, len(list)):
-    print(list[i])
-    if list[i] == 'b':
-        print('Beata')
+# list = ['a', 'b', 'c', 'd']
+# for i in range(0, len(list)):
+#     print(list[i])
+#     if list[i] == 'b':
+#         # print('Beata')
+
+# import random
+# a = 3
+# b = int(3*a)
+# print(random.randint(a, b))
+
+# import random
+# amount = random.randint(1, 15)
+# text = 'cookie'
+# print(amount*text)
+
+# import random
+# points = 0
+# while True:
+#     n1 = random.randint(0, 101)
+#     n2 = random.randint(0, 101)
+#     print('n1 =',n1)
+#     print('n2 =',n2)
+#     difference = n1 - n2
+#     print('n1 - n2 = ?')
+#     print('Give me a result!')
+#     user = int(input())
+#     if difference == user:
+#         print('Correct!')
+#         points += 1
+#     else:
+#         print('Wrong!')
+#         points -= 1
+#     print('n1 - n2 = ' + str(difference))
+#     print('Your streak: ' + str(points))
+#     continue
