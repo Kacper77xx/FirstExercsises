59816dfdb4997156c9b805625ad4e5d0ae7b6ceb
Kacper <kacper77xx@gmail.com>
2023-04-24 15:38:23 +0200

    First commit

--- Exercise001.py
+++ Exercise001.py
@@ -49,21 +49,57 @@
 # else:
 #     print(int(False))
 # print('You received: ' + str(points))
-import random
-n1 = random.randint(1, 101)
-n2 = random.randint(1, 101)
-n3 = random.randint(1, 101)
-print(str(n1) + ' ' + str(n2) + ' ' + str(n3))
-userChoice = int(input('Choose the highest number! '))
-if userChoice == n1:
-    if n1 > n2:
-        if n1 > n3:
-            print('Correct!')
-if userChoice == n2:
-    if n2 > n1:
-        if n2 > n3:
-            print('Correct!')
-if userChoice == n3:
-    if n3 > n2:
-        if n3 > n1:
-            print('Correct!')
+# import random
+# n1 = random.randint(1, 101)
+# n2 = random.randint(1, 101)
+# n3 = random.randint(1, 101)
+# print(str(n1) + ' ' + str(n2) + ' ' + str(n3))
+# userChoice = int(input('Choose the highest number! '))
+# if userChoice == n1:
+#     if n1 > n2:
+#         if n1 > n3:
+#             print('Correct!')
+#     else:
+#         print('Not this time')
+# if userChoice == n2:
+#     if n2 > n1:
+#         if n2 > n3:
+#             print('Correct!')
+#     else:
+#         print('Not this time')
+# if userChoice == n3:
+#     if n3 > n2:
+#         if n3 > n1:
+#             print('Correct!')
+#     else:
+#         print('Not this time')
+
+# print('What is your school grade?')
+# grade = int(input())
+# if 2 <= grade <= 6:
+#     print('Passed')
+# else:
+#     print('Unlucky, sorry')
+
+
+# uText = input('Give me a random text! ')
+# if uText.find('!') >= 0 or uText.find('@') >= 0 or uText.find('$') >= 0 or uText.find('#') >= 0:
+#     print('I found special word!')
+# else:
+#     print('There is no a special word!')
+
+# if 16 < 7 or 8 != 4+4:
+#     print('a')
+# elif True:
+#     print('b')
+# else:
+#     print('c')
+
+sum = 1
+while True:
+    print('*' * sum)
+    sum += 1
+    if sum == 10:
+        break
+print('Over')
+
